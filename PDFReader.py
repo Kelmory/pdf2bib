75692a6c42ae788c2cd27e624cc6aadc7db9fdb2
kelmory <kelmory@163.com>
2018-06-29 15:47:11 +0800

    Merge problems fixed.

--- PDFReader.py
+++ PDFReader.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-from . import Spider
+import logging
 
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal, LAParams
@@ -7,7 +6,8 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
 from pdfminer.pdfparser import PDFParser, PDFDocument
 
-import logging
+from . import Spider
+
 logging.propagate = False
 logging.getLogger('pdfminer').setLevel(logging.ERROR)
 
@@ -62,68 +62,3 @@ class PDFReader:
                         if isinstance(out, LTTextBoxHorizontal):
                             buffer.append(out.get_text())
             return buffer
-=======
-from . import Spider
-
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LTTextBoxHorizontal, LAParams
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
-from pdfminer.pdfparser import PDFParser, PDFDocument
-
-import logging
-logging.propagate = False
-logging.getLogger('pdfminer').setLevel(logging.ERROR)
-
-
-class PDFReader:
-    def __init__(self, _path):
-        self._path = _path
-        self._online = self.is_online()
-
-    def is_online(self):
-        if self._path.find('http') >= 0:
-            return True
-        else:
-            return False
-
-    def parse(self, password=None):
-        if not self._online:
-            try:
-                fp = open(self._path, 'rb')
-            except:
-                raise FileNotFoundError
-        else:
-            try:
-                fp = Spider(self._path).get_from_url()
-            except:
-                raise FileNotFoundError
-
-        parser_pdf = PDFParser(fp)
-        doc = PDFDocument()
-        parser_pdf.set_document(doc)
-        doc.set_parser(parser_pdf)
-
-        if password is not None:
-            doc.initialize(password)
-        else:
-            doc.initialize()
-
-        if not doc.is_extractable:
-            print('Not extractable.')
-            raise PDFTextExtractionNotAllowed
-        else:
-            rsrcmgr = PDFResourceManager()
-            laparams = LAParams()
-            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-            interpreter = PDFPageInterpreter(rsrcmgr, device)
-            buffer = []
-            for page in doc.get_pages():
-                interpreter.process_page(page)
-                layout = device.get_result()
-                for out in layout:
-                    if hasattr(out, "get_text"):
-                        if isinstance(out, LTTextBoxHorizontal):
-                            buffer.append(out.get_text())
-            return buffer
->>>>>>> 46431f867d190d95d5b4df43f1fa349b9eb11098
--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PREI import pdf2ref
 
 
@@ -11,17 +10,3 @@ if __name__ == '__main__':
         if rec is not None:
             count += 1
     print('Successfully get bibtex: %d / %d' % (count, total))
-=======
-from PREI import pdf2ref
-
-
-if __name__ == '__main__':
-    extractor = pdf2ref.RefExtractor('F:/review.pdf', scholar='dblp')
-    extractor.extract()
-    total = len(extractor.records)
-    count = 0
-    for rec in extractor.records:
-        if rec is not None:
-            count += 1
-    print('Successfully get bibtex: %d / %d' % (count, total))
->>>>>>> 46431f867d190d95d5b4df43f1fa349b9eb11098
